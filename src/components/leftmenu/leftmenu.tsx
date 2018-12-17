68d07486ad29484a394e4c3c582c94e08559c3e5
Stephan Dittmann <info@dessounds.com>
2018-12-17 18:14:08 +0100

    edit left menu

--- src/components/leftmenu/leftmenu.tsx
+++ src/components/leftmenu/leftmenu.tsx
@@ -1,7 +1,7 @@
 import './leftmenu.scss';
 import React from 'react';
-// import { NavLink } from 'react-router-dom';
-import { NavItem, NavLink, Nav } from 'reactstrap';
+// import {} from 'react-router-dom';
+import {NavItem, Nav, NavLink  } from 'reactstrap';
 
 interface LeftMenuState {}
 interface LeftMenuProps {}
@@ -9,23 +9,23 @@ interface LeftMenuProps {}
 export class LeftMenu extends React.Component<LeftMenuProps, LeftMenuState> {
 	public render(): React.ReactNode {
 		return (
-				<Nav className="main-menu" vertical>
-					<NavItem className="menu-link">
-						<NavLink to="/home">Anschreiben</NavLink>
-					</NavItem>
-					<NavItem className="menu-link">
-						<NavLink to="/cv">CV</NavLink>
-					</NavItem>
-					<NavItem className="menu-link">
-						<NavLink to="/stats">Stats</NavLink>
-					</NavItem>
-					<NavItem className="menu-link">
+			<Nav className="main-menu" vertical>
+				<NavItem className="menu-link">
+					<NavLink to="/home">Anschreiben</NavLink>
+				</NavItem>
+				<NavItem className="menu-link">
+					<NavLink to="/cv">CV</NavLink>
+				</NavItem>
+				<NavItem className="menu-link">
+					<NavLink to="/stats">Stats</NavLink>
+				</NavItem>
+				<NavItem className="menu-link">
 					<NavLink to="/work">Work</NavLink>
-					</NavItem>
-					<NavItem className="menu-link">
+				</NavItem>
+				<NavItem className="menu-link">
 					<NavLink to="/zeugnisse">Zeugnisse</NavLink>
-					</NavItem>
-				</Nav>
+				</NavItem>
+			</Nav>
 		);
 	}
 }
